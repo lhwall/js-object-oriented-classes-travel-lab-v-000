03cd978971072226268379b78cd7253617909330
Lucy Wall <lhwall@gmail.com>
2018-11-28 16:40:35 +0000

    Done.

--- index.js
+++ index.js
@@ -5,7 +5,28 @@ class Driver {
   }
 
   yearsExperienceFromBeginningOf(endDate){
-    new Date(endDate) - this.startDate
+    let time = endDate - this.startDate.getFullYear()
+    return time
+  }
+
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+
+  blocksTravelled(){
+    let verticalBeginning = Number(this.beginningLocation.vertical)
+    let horizontalBeginning = Number(this.beginningLocation.horizonal)
+    let verticalEnding = Number(this.beginningLocation.vertical)
+    let horizontalEnding = Number(this.beginningLocation.horizonal)
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+
+    let verticalTotal = Math.abs(verticalBeginning - verticalEnding)
+    let horizontalTotal = Math.abs()
   }
 
 }
